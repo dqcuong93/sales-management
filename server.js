5f5c4eb9f545ed9804ef801eea13439c6c1adab0
dqcuong93 <dqcuong93@yahoo.com.vn>
2017-05-19 23:18:11 +0700

    Get home page

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 var express = require('express');
-var app = express()
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('You have requested home page !');
+});
 
 var server = app.listen(8080, function () {
     console.log('Server has started on port ' + server.address().port);
