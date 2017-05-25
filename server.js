ff40ff75fc296ca7214041e478f32fd850147ea5
dqcuong93 <dqcuong93@yahoo.com.vn>
2017-05-25 14:47:32 +0700

    Trying to get request info

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 var express = require('express');
 var app = express();
 var db = require('./db');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var upload = multer({dest: 'uploads/'});
 
 app.set('views', './views');
 app.set('view engine', 'pug');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(express.static('public'));
+app.use(upload.array());
 
 app.get('/', function (req, res) {
     res.render('home');
+});
+
+app.post('/home', function (req, res) {
+    console.JSON(req.body);
 });
 
 var server = app.listen(8080, function () {
